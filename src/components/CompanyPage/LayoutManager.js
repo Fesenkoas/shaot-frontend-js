f7021374f79933e6c00b1ef817268882893857e3
Aleksandr Fesenko <fesenkoas@gmail.com>
2023-08-10 21:02:44 +0300

    new

--- src/components/CompanyPage/LayoutManager.js
+++ src/components/CompanyPage/LayoutManager.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Sidebar } from "./SideBar";
+import { Header } from "./Header";
 
 export const LayoutManadger = ({ children }) => {
   return (
     <React.Fragment>
       <div className="body">
-        <Sidebar />
+        <Header/>
+        {/* <Sidebar /> */}
         {children}
       </div>
     </React.Fragment>
