328cfff5d6cf1385da0a6f36b6654ce6dbd8bcf0
Aleksandr Fesenko <fesenkoas@gmail.com>
2023-08-14 19:47:22 +0300

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-<<<<<<< HEAD
     <footer className="flex  h-20 py-4 justify-between w-full  bg-[#99C2BD] absolute bottom-0">
       
     </footer>
-=======
-    <footer className="flex  h-20 py-4 justify-between items-center bg-[#99C2BD]"/>
->>>>>>> 025ed5d151963df12a571d378e652fe7e4f5cd5c
   )
 }
