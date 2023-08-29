7e36c659ccd16927d0c2c6b82d84a643de72998e
Aleksandr Fesenko <fesenkoas@gmail.com>
2023-08-29 20:17:00 +0300

    new

--- src/pages/CompanyPage/Company.js
+++ src/pages/CompanyPage/Company.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { FaTrashCan } from "react-icons/fa6";
 import { BsCalendar2Plus, BsTrash3 } from "react-icons/bs";
 import {
   getWeekConfigurate,
